3ec0c8b47e7c273220a5706bb33c9dc2d5e9be9f
Lucas Mandelli <lucasmandelli@gmail.com>
2016-10-14 13:04:06 -0300

    Eventos, Pacotes externos e Publicação

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ var handle = require('./handle');
 
 var server = http.createServer(handle);
 
+require('console-log-hello-world');
+
 server.listen(3000, function() {
     console.log('Server is listening at port 3000');
 });
+
+
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+emitter.on('say12', say);
+
+function say() {
+    console.log('Im saying...');
+}
+
+emitter.emit('say12');
--- handle.js
+++ handle.js
@@ -1,7 +1,22 @@
+var queryString = require('querystring');
+
 function handle(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
+
+    var body = '';
+
+    request.on('data', function(data) {
+        body += data.toString();
+    });
+
+    request.on('end', function() {
+        var decode = queryString.parse(body);
+
+        console.log(decode);
+    });
+
     response.write('<!DOCTYPE "html">');
     response.write('<html>');
     response.write('<head><title>Http Module</title></head>');
